cda7400225a5c3844e90e2f512ad738e0af66d54
omirfarhan <omirfarhan1@gmail.com>
2026-08-11 15:58:07 +0600

    pyhon data type

--- demo.py
+++ demo.py
@@ -2,7 +2,6 @@ import random
 options = ["rock", "paper", "scissors"]
 def get_choice():
     player_choice = input("enter a choice (rock, paper, scissors): ")
-    
     computer_choice = random.choice(options)
     choices = {"player": player_choice, "computer": computer_choice}
     return choices
---
+++ part1.py
@@ -0,0 +1,5 @@
+#this is code editor
+name = "Turjoy Islam Omi"
+print(isinstance(name,str))
+age = 23
+print(isinstance(age, int))
